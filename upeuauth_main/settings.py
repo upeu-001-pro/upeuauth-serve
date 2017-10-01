8467f536e9be5d8d1104980a119acab36b3f8c86
Angel Sullon <asullom@gmail.com>
2017-10-01 17:33:47 -0500

    Access-Control-Allow-Origin

--- upeuauth_main/settings.py
+++ upeuauth_main/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # adding
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
